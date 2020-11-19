2343202d140b5dc5879af3001b35a0856f5aea6b
Rebbit <hyperaktiv99@gmail.com>
2020-11-19 16:59:51 +0700

    nop bai

--- baiB/baiB.js
+++ baiB/baiB.js
@@ -2,73 +2,68 @@ const api_url =
    "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple";
 let myQuestions = [];
 
-(function () {
-
-   function buildQuiz() {
-      const output = [];
-      myQuestions.forEach(
-         (currentQuestion, questionNumber) => {
-            const answers = [];
-            for (letter in currentQuestion.answers) {
-               answers.push(
-                  `<label>
+function buildQuiz() {
+   const output = [];
+   myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+         const answers = [];
+         for (letter in currentQuestion.answers) {
+            answers.push(
+               `<label>
                <input type="radio" name="question${questionNumber}" value="${letter}">
                ${letter} :
                ${currentQuestion.answers[letter]}
              </label>`
-               );
-            }
-            output.push(
-               `<div class="question"> ${currentQuestion.question} </div>
-           <div class="answers"> ${answers.join('')} </div>`
             );
          }
-      );
-      quizContainer.innerHTML = output.join('');
-   }
+         output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
+           <div class="answers"> ${answers.join('')} </div>`
+         );
+      }
+   );
+   quizContainer.innerHTML = output.join('');
+}
 
-   function showResults() {
-
-
-      const answerContainers = quizContainer.querySelectorAll('.answers');
-
-      let numCorrect = 0;
-
-      myQuestions.forEach((currentQuestion, questionNumber) => {
-         const answerContainer = answerContainers[questionNumber];
-         const selector = `input[name=question${questionNumber}]:checked`;
-         const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-         if (userAnswer === currentQuestion.correctAnswer) {
-            numCorrect++;
-            answerContainers[questionNumber].style.color = 'lightgreen';
-         }
-         else {
-            answerContainers[questionNumber].style.color = 'red';
-         }
-      });
-
-      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-   }
+function showResults() {
 
 
+   const answerContainers = quizContainer.querySelectorAll('.answers');
 
-   const quizContainer = document.getElementById('quiz');
-   const resultsContainer = document.getElementById('results');
-   const submitButton = document.getElementById('submit');
+   let numCorrect = 0;
 
-   async function getapi(url) {
-      const response = await fetch(url);
-      var data = await response.json();
+   myQuestions.forEach((currentQuestion, questionNumber) => {
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-      myQuestions = data.results;
+      if (userAnswer === currentQuestion.correctAnswer) {
+         numCorrect++;
+         answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      else {
+         answerContainers[questionNumber].style.color = 'red';
+      }
+   });
 
-      console.log(myQuestions)
-      buildQuiz();
+   resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
 
-      submitButton.addEventListener('click', showResults);
-   }
 
-   getapi(api_url);
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
 
-})();
+async function getapi(url) {
+   const response = await fetch(url);
+   var data = await response.json();
+
+   myQuestions = data.results;
+
+   console.log(myQuestions)
+   buildQuiz();
+
+   submitButton.addEventListener('click', showResults);
+}
+
+getapi(api_url);
